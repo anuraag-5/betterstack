bdab43db07a1d24082b158ff4aade7c549624c35
Anurag bhoite <anuragcr7778@gmail.com>
2025-06-16 00:38:38 +0530

    ...

--- apps/api/routes/v1/userRouter.ts
+++ apps/api/routes/v1/userRouter.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import express from "express";
 
 const userRouter = Router();
+userRouter.use(express.json());
 
 userRouter.get("/", (req, res) => {
     res.json({
--- apps/api/routes/v1/websiteRouter.ts
+++ apps/api/routes/v1/websiteRouter.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import client from "store/client";
+import express from "express";
 
 const websiteRouter = Router();
 
-websiteRouter.get("/", (req, res) => {
-  res.send("Welcome to the website router");
+websiteRouter.use(express.json());
+
+websiteRouter.post("/", async (req, res) => {
+  const newWebsite = await client.website.create({
+    data: {
+      timeAdded: new Date(),
+      url: req.body.url
+    }
+  });
+
+  res.json({
+    id: newWebsite.id
+  });
 });
 
 websiteRouter.get("/:websiteID/status", (req, res) => {
